3e008b1d7ed7214016e82a5858910e5c5afa19fe
Tho Ho <code@tho.email>
2021-08-25 00:07:20 +0800

    Update test production

--- NetLah.Extensions.Configuration.Test/ConfigurationBuilderBuilderTest.cs
+++ NetLah.Extensions.Configuration.Test/ConfigurationBuilderBuilderTest.cs
@@ -127,6 +127,7 @@ namespace NetLah.Extensions.Configuration
             });
 
             Assert.Equal("Production", builder.EnvironmentName);
+            AssertProduction(configuration);
         }
 
         [Fact]
