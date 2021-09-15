29339a0400ae9995554fd2313c849c315ab4758b
Tho Ho <code@tho.email>
2021-09-15 19:23:25 +0800

    Support multi TargetFrameworks: net6.0, net5.0, netstandard2.1; update package tags

--- samples/ConsoleApp/Program.cs
+++ samples/ConsoleApp/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.Extensions.Configuration;
+using NetLah.Diagnostics;
 using NetLah.Extensions.Configuration;
 
 namespace ConsoleApp
@@ -10,6 +11,18 @@ namespace ConsoleApp
     {
         public static void Main(string[] args)
         {
+            var appInfo = ApplicationInfo.Initialize(null);
+            Console.WriteLine($"AppTitle: {appInfo.Title}");
+            Console.WriteLine($"Version:{appInfo.InformationalVersion} BuildTime:{appInfo.BuildTimestampLocal}; Framework:{appInfo.FrameworkName}");
+
+            var asmAbstracts = new AssemblyInfo(typeof(AssemblyInfo).Assembly);
+            Console.WriteLine($"AssemblyTitle: {asmAbstracts.Title}");
+            Console.WriteLine($"Version:{asmAbstracts.InformationalVersion} BuildTime:{asmAbstracts.BuildTimestampLocal}; Framework:{asmAbstracts.FrameworkName}");
+
+            var asmConfig = new AssemblyInfo(typeof(ConfigurationBuilderBuilder).Assembly);
+            Console.WriteLine($"AssemblyTitle: {asmConfig.Title}");
+            Console.WriteLine($"Version:{asmConfig.InformationalVersion} BuildTime:{asmConfig.BuildTimestampLocal}; Framework:{asmConfig.FrameworkName}");
+
             var configuration = ConfigurationBuilderBuilder.Create<Program>(args).Build();
             var defaultConnectionString = configuration.GetConnectionString("DefaultConnection");
             Console.WriteLine($"[TRACE] ConnectionString: {defaultConnectionString}");
