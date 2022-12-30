939ace10ebb0396468094ba5b2aa713b4983a6cd
Tho Ho <code@tho.email>
2022-12-30 22:29:37 +0800

    Add enum flags parsing tests

--- NetLah.Extensions.Configuration.Test/EnumParseBindTest.cs
+++ NetLah.Extensions.Configuration.Test/EnumParseBindTest.cs
@@ -4,7 +4,7 @@ using Xunit;
 
 namespace NetLah.Extensions.Configuration.Test;
 
-public class EnumBindTest
+public class EnumParseBindTest
 {
     private static IConfiguration NewConfig(Dictionary<string, string?> initialData)
         => new ConfigurationBuilder()
@@ -58,11 +58,11 @@ public class EnumBindTest
     [InlineData("UserKeySet,MachineKeySet,Exportable,UserProtected", 15)]
     [InlineData("UserKeySet,MachineKeySet,Exportable,UserProtected,UserProtected", 15)]
     [InlineData("UserKeySet,MachineKeySet,Exportable,UserProtected,PersistKeySet,EphemeralKeySet", 63)]
-    public void ZeroDefault(string strValue, int expectedValue)
+    public void ConfigurationBind(string input, int expectedValue)
     {
         var config = NewConfig(new Dictionary<string, string?>
         {
-            ["KeyStorageFlags"] = strValue,
+            ["KeyStorageFlags"] = input,
         });
 
         var options = config.Get<MyOptions>();
@@ -70,6 +70,33 @@ public class EnumBindTest
         Assert.NotNull(options);
         Assert.True(options.KeyStorageFlags.HasValue);
         Assert.Equal(expectedValue, (int)options.KeyStorageFlags.Value);
+    }
+
+    [Theory]
+    [MemberData(nameof(IntegerData))]
+    [InlineData("DefaultKeySet", 0)]
+    [InlineData("UserKeySet", 1)]
+    [InlineData("MachineKeySet", 2)]
+    [InlineData("Exportable", 4)]
+    [InlineData("UserProtected", 8)]
+    [InlineData("PersistKeySet", 16)]
+    [InlineData("EphemeralKeySet", 32)]
+    [InlineData("UserKeySet,MachineKeySet", 3)]
+    [InlineData("DefaultKeySet,UserKeySet,MachineKeySet", 3)]
+    [InlineData("MachineKeySet,UserKeySet", 3)]
+    [InlineData("UserKeySet,MachineKeySet,Exportable,UserProtected", 15)]
+    [InlineData("UserKeySet,MachineKeySet,Exportable,UserProtected,UserProtected", 15)]
+    [InlineData("UserKeySet,MachineKeySet,Exportable,UserProtected,PersistKeySet,EphemeralKeySet", 63)]
+    public void EnumParse(string input, int expectedValue)
+    {
+        if (Enum.TryParse<X509KeyStorageFlags>(input, out var value))
+        {
+            Assert.Equal(expectedValue, (int)value);
+        }
+        else
+        {
+            Assert.Fail("Cannot parse: " + input);
+        }
     }
 
     internal class MyOptions
